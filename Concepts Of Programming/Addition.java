db79ef2a3f09346e766df6e4af43f2b0cd154bbd
Abdul Ahad Sheikh <95898825+gitabdulahad@users.noreply.github.com>
2023-04-01 13:49:57 +0530

    Relevant comments added

--- Concepts Of Programming/Addition.java
+++ Concepts Of Programming/Addition.java
@@ -6,10 +6,14 @@ public class Addition {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter the numbers to add: \n");
+        // asking user for inputs
         int a = in.nextInt();
         int b = in.nextInt();
-
+        
+        // performing addition
         int c = a + b;
+        
+        //printing output
         System.out.println("The sum of the two numbers is: " + c);
     }
 }
