900186379c840272b6ee6aba02a150eebcccd39f
Abdul Ahad Sheikh <95898825+gitabdulahad@users.noreply.github.com>
2023-04-01 14:18:25 +0530

    Update and rename HelloWorld.java to 1.HelloWorld.java

--- Concepts Of Programming/1.HelloWorld.java
+++ Concepts Of Programming/1.HelloWorld.java
@@ -2,6 +2,7 @@ package ConceptsOfProgramming.Assignment_1;
 
 public class HelloWorld {
     public static void main(String[] args) {
+        //printing output
         System.out.println("Hello World");
     }
 }
