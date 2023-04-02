4c855c1328412f95b3413f48372dbe55274f86a2
Abdul Ahad Sheikh <95898825+gitabdulahad@users.noreply.github.com>
2023-04-02 17:40:50 +0530

    Solution to Question 3. uploaded

--- Concepts Of Programming/Assignment_1/Swap.java
+++ Concepts Of Programming/Assignment_1/Swap.java
@@ -6,12 +6,14 @@ public class Swap {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter your numbers: ");
-
+        
+        //taking inputs from user
         int a = in.nextInt();
         int b = in.nextInt();
 
         System.out.println("Your numbers before Swap are: \n" + a + " " + b);
-
+        
+        //mathematical operation to swap numbers without using third variable
         a = a+b;
         b = a-b;
         a = a-b;
