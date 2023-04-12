513272c3055ea56decfd46a2356c130dbcfffd46
Abdul Ahad Sheikh <95898825+gitabdulahad@users.noreply.github.com>
2023-04-12 21:21:22 +0530

    Update 01. HelloWorld.java
    Signed-off-by: Abdul Ahad Sheikh <95898825+gitabdulahad@users.noreply.github.com>

--- Concepts Of Programming/Assignment_1/01. HelloWorld.java
+++ Concepts Of Programming/Assignment_1/01. HelloWorld.java
@@ -3,6 +3,6 @@ package ConceptsOfProgramming.Assignment_1;
 public class HelloWorld {
     public static void main(String[] args) {
         //printing output
-        System.out.println("Hello World!");
+        System.out.println("Hello World");
     }
 }
