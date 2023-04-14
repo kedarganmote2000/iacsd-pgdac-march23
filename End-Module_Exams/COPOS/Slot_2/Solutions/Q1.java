9d24cc9375d9b00cf3939e9952f997539540b5af
Kedar_Ganmote <kedarganmote2000@gmail.com>
2023-04-14 14:27:02 +0530

    Update Q1.java
    Signed-off-by: Kedar_Ganmote <kedarganmote2000@gmail.com>

--- End-Module_Exams/COPOS/Slot_2/Solutions/Q1.java
+++ End-Module_Exams/COPOS/Slot_2/Solutions/Q1.java
@@ -19,7 +19,7 @@ public class COPOSSolutionQ3 {
       // Checking the occurance of each element in the array
                 for (int i = 0; i < arr.length; i++) {
                     int count = 0;
-                  
+                    i!=arr.length-1;
                     for (int k = 0; k < arr.length; k++) {
                         if (arr[i] == arr[k])
                             count++;
@@ -27,6 +27,13 @@ public class COPOSSolutionQ3 {
                   
                   // Printing out the occurance of each element in the array
                     System.out.println(arr[i] + " occurs " + count + " times in the Array");
+                    i+=(count-1);
+                           //It is used to shift the i to the last Position
+		                  //of the Occurance element or be on the same position
+	                      //for single Element 
+			              //for eg (4 4 4 5 2) here i=0,but after i value will become 2
+			              // i.e it will go to last position of the same element
+			              //  and when i will 3 it will be on the same position i.e i=3
                 }
     }
 }
