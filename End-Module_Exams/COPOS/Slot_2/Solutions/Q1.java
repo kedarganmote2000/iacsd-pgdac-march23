88390a21d99dcaa6e92a3c9f0ea125e8fcd3fe2e
Abdul Ahad Sheikh <95898825+gitabdulahad@users.noreply.github.com>
2023-04-14 13:07:05 +0530

    Create Q1.java
    Signed-off-by: Abdul Ahad Sheikh <95898825+gitabdulahad@users.noreply.github.com>

---
+++ End-Module_Exams/COPOS/Slot_2/Solutions/Q1.java
@@ -0,0 +1,34 @@
+// Accept 10 integers from user and count the occurrences of each element in the array.
+
+import java.util.Scanner;
+
+public class COPOSSolutionQ3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int[] arr = new int[10];
+
+        System.out.println("Enter the 10 elements of your array: ");
+      
+      // Asking user for inputs using for loop
+      for (int i = 0; i < arr.length; i++) {
+            System.out.println("Element " + (i + 1) + ": ");
+            arr[i] = in.nextInt();
+        }
+      
+      // Checking the occurance of each element in the array
+                for (int i = 0; i < arr.length; i++) {
+                    int count = 0;
+                  
+                  // write your logic for check condition here:
+                  
+                    for (int k = 0; k < arr.length; k++) {
+                        if (arr[i] == arr[k])
+                            count++;
+                    }
+                  
+                  // Printing out the occurance of each element in the array
+                    System.out.println(arr[i] + " occurs " + count + " times in the Array");
+                }
+    }
+}
